166524de0fad171b3c52a57f5f27e3ebfcc19ff0
Sheen An <sheen.an.goh@gmail.com>
2023-07-09 22:55:58 +0800

    init understanding on ownership

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use std::io;
+
 fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+
     let mut mars_weight: f32 = calculate_weight_on_mars(100.0);
     mars_weight = mars_weight * 1000.0;
     println!("Weight on Mars: {}g", mars_weight);
@@ -7,3 +12,8 @@ fn main() {
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
 }
+
+// Ownership Rules
+// 1. Each value in Rust is owned by a variable
+// 2. When the owner goes out of scope, the value will be deallocated.
+// 3. There can only be ONE owner at a time.
